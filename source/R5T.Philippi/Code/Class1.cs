d57310580a1a5ae4365bde098677529f9d2407aa
MinexAutomation <minex.automation@gmail.com>
2019-11-01 14:03:27 -0700

    Added file format enumeration.

--- source/R5T.Philippi/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Philippi
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Philippi/Code/Enumerations/FileFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace R5T.Philippi
+{
+    public enum FileFormat
+    {
+        Unknown = 0,
+
+        // Image-related.
+        BMP,
+        JPG, // Also includes JPEG.
+        PNG,
+    }
+}
